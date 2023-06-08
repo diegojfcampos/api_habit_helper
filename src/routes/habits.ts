b0e5779f3839dc2e885aa581f80777ad01d7eb76
diegojfcampos <diegojfcampos@gmail.com>
2023-06-08 01:12:16 +0100

    update: summary route

--- src/routes/habits.ts
+++ src/routes/habits.ts
@@ -127,11 +127,11 @@ async function habitsRoutes(app: FastifyInstance, options: any, done: () => void
           SELECT
             COUNT(*)
           FROM habit_week_days HWD
-            JOIN habits H ON H.id = HWD.habit_id
+          JOIN habits H ON H.id = HWD.habit_id
           WHERE HWD.week_day = EXTRACT(DOW FROM D.date)
-            AND H."createdAt" < D.date
+            AND H."createdAt" < D.date::date
         ) as amount
-      FROM days D
+      FROM days D;
     `;
   
     reply.send({ summary });

ccab4b68ba2d907c54818721d8d710b8bccafaf7
diegojfcampos <diegojfcampos@gmail.com>
2023-06-09 19:32:23 +0100

    update: build deploy

--- dist/routes/habits.js
+++ dist/routes/habits.js
@@ -104,23 +104,27 @@ async function habitsRoutes(app, options, done) {
         D.date,
         (
           SELECT 
-            cast(count(*) as float)
+            COUNT(*)
           FROM day_habits DH
           WHERE DH.day_id = D.id
         ) as completed,
         (
           SELECT
-            cast(count(*) as float)
-            FROM habit_week_days HWD
-            JOIN habits H
-              ON H.id = HWD.habit_id
-            WHERE HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
-            AND H.createdAt < D.date
-        ) as amount
-      FROM days D
-
-      `;
-        reply.send({ summary });
+            COUNT(*)
+          FROM habit_week_days HWD
+              JOIN habits H ON H.id = HWD.habit_id
+          WHERE HWD.week_day = EXTRACT(DOW FROM D.date)
+              AND H."createdAt" < D.date::date
+        )::integer as amount
+      FROM days D;
+    `;
+        // Convert BigInt values to regular integers
+        const formattedSummary = summary.map((item) => ({
+            ...item,
+            completed: Number(item.completed),
+            amount: Number(item.amount)
+        }));
+        reply.send({ summary: formattedSummary });
     });
     done();
 }
